10fb2772f40557233d2342490c34237c3b93f552
mkd92 <manikandan.m1116@gmail.com>
2022-06-05 23:45:36 +0530

    passport config completed but havent checked

--- src/passportConfig.js
+++ src/passportConfig.js
@@ -1,3 +1,28 @@
-function initialize(passport) {}
+const { UserModel } = require("./mongodb/model/userModel");
+
+function initialize(passport) {
+  var JwtStrategy = require("passport-jwt").Strategy,
+    ExtractJwt = require("passport-jwt").ExtractJwt;
+  var opts = {};
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+  opts.secretOrKey = "secret";
+  opts.issuer = "accounts.examplesoft.com";
+  opts.audience = "yoursite.net";
+  passport.use(
+    new JwtStrategy(opts, function (jwt_payload, done) {
+      UserModel.findOne({ id: jwt_payload.sub }, function (err, user) {
+        if (err) {
+          return done(err, false);
+        }
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+          // or you could create a new account
+        }
+      });
+    })
+  );
+}
 
 module.exports = initialize;
