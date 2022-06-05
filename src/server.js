395a350e6ed0f78ea59fbd2ee5eb7a5592f0957a
mkd92 <manikandan.m1116@gmail.com>
2022-06-05 23:15:51 +0530

    Mongodb connected

--- src/server.js
+++ src/server.js
@@ -1,11 +1,24 @@
 const express = require("express");
-const authRoute = require("./router/auth-route");
+const passport = require("passport");
+const mongoose = require("mongoose");
 
+const authRoute = require("./router/authRoute");
 require("dotenv").config();
+const initializePassport = require("./passportConfig");
+
 app = express();
+initializePassport(passport);
 
 app.use("/auth", authRoute);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server started in port : ${process.env.PORT}`);
+app.listen(process.env.PORT, async () => {
+  await mongoose
+    .connect("mongodb://localhost:27017/lobby-express-auth")
+    .then(() => {
+      console.log("Mongodb connected");
+      console.log(`Server started in port : ${process.env.PORT}`);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 });
